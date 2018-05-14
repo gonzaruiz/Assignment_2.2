e7dea827f9fd7bc5f49c990d9f082b685dd2913e
gonzalo ruiz <38412697+gonzaruiz@users.noreply.github.com>
2018-05-14 09:51:11 -0500

    Add files via upload

---
+++ Assignment_2.2.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[20]:
+
+
+a = 3
+b = 3
+c = 4
+d = 5
+e = 6
+f = 7
+
+if a==b:
+    print('*')
+
+if a<b:
+    print('**')
+    
+if a<d:
+    print('***')
+
+if a<e:
+    print('****')
+    
+if a<f:
+    print('*****')
+
+if a<c:
+    print('****')
+    
+if a<d:
+    print('***')   
+    
+if a<c:
+    print('**')
+    
+if a==b:
+    print('*')
+    
+    
+    
+
